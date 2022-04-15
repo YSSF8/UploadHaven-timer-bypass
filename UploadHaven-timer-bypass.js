17f841cf712f98c17a9ed7d9edcf4c6781443854
YSSF8 <76586183+YSSF8@users.noreply.github.com>
2022-04-15 17:38:28 +0100

    Add files via upload

---
+++ UploadHaven-timer-bypass.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         UploadHaven Timer Bypass
+// @version      1.0
+// @description  Bypasses the UploadHaven download timer
+// @author       YSSF
+// @match        https://uploadhaven.com/download/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    seconds = 0;
+
+    //Original script ↓↓↓
+    /*
+    let seconds = 0;
+    let countdownTimer;
+
+    function countDown() {
+    if (seconds === 0) {
+        $('.btn-submit-free').html('Free Download');
+        $('.btn-submit-free').attr('type', 'submit');
+
+        clearTimeout(countdownTimer);
+    } else {
+        $('.download-timer-seconds').text(seconds + ' seconds');
+    }
+
+    seconds--;
+    }
+
+    $(document).ready(function() {
+        function startCountdown() {
+            $(this).attr('disabled', true);
+
+            countdownTimer = setInterval('countDown()', 1000);
+        }
+
+        startCountdown();
+    });*/
+})();
